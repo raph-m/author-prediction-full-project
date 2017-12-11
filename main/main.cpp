46baee8f8dd6cc97254f05c38e46134714fc3bfe
raph-m <raphael.montaud@riminder.net>
2017-12-11 16:18:44 +0100

    initial commit

---
+++ main/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+//#include "Classification/distances.h"
+//#include "Classification/column.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "ca compile ouais !" << std::endl;
+    return 0;
+}
